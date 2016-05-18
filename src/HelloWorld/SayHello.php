11badfe9f02d5b77a8328ec9b22c50b8ff1be5e6
Maulik <km8295@gmail.com>
2016-05-18 12:34:55 +0530

    first commit

---
+++ src/HelloWorld/SayHello.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace HelloWorld;
+/**
+ * Description of SayHello
+ *
+ * @author office 3
+ */
+class SayHello {
+    //put your code here
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
